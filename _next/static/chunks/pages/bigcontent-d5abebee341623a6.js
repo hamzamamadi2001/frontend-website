fe6ae215a47e6cea107539f0ddd84ee161541da1
hamzamamadi2001 <hamzamamadi20@gmail.com>
2022-04-18 13:24:48 +0100

    ok

---
+++ _next/static/chunks/pages/bigcontent-d5abebee341623a6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[635],{3729:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/bigcontent",function(){return u(1812)}])}},function(n){n.O(0,[812,774,888,179],(function(){return _=3729,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/news-3468a17638f3eb76.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[134],{6631:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/news",function(){return u(3450)}])}},function(n){n.O(0,[675,814,450,774,888,179],(function(){return _=6631,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
